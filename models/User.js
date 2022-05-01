9881ef5bfc6ce2f4b098379e7afe850d0b04f153
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-05-01 00:26:56 -0400

    Base user model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(email) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email)
+                },
+                message: 'Not a valid email'
+            }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thoughts"
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: this
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+)
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
